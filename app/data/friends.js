1e512de1a92d7411240a9ab86056899b0d9a2df1
slizbethellis <slizbethellis@gmail.com>
2017-12-20 07:55:23 -0600

    friends data, html layout, and survey questions

---
+++ app/data/friends.js
@@ -0,0 +1,80 @@
+var friends = [
+  {
+    "name":"Maurice Moss",
+    "photo":"assets/images/maurice-moss.png",
+    "scores":[
+        2,
+        5,
+        1,
+        5,
+        5,
+        5,
+        4,
+        1,
+        1,
+        3
+      ]
+  }, {
+    "name":"Roy Trenneman",
+    "photo":"assets/images/roy-trenneman.jpg",
+    "scores":[
+        1,
+        4,
+        2,
+        4,
+        3,
+        1,
+        4,
+        4,
+        2,
+        1
+      ]
+  }, {
+    "name":"Jen Barber",
+    "photo":"assets/images/jen-barber.jpg",
+    "scores":[
+        5,
+        2,
+        5,
+        1,
+        1,
+        3,
+        5,
+        3,
+        4,
+        5
+      ]
+  }, {
+    "name":"Richmond Avenal",
+    "photo":"assets/images/richmond-avenal.jpg",
+    "scores":[
+        3,
+        1,
+        3,
+        2,
+        4,
+        2,
+        1,
+        2,
+        3,
+        2
+      ]
+  }, {
+    "name":"Douglas Reynholm",
+    "photo":"assets/images/douglas-reynholm.jpg",
+    "scores":[
+        4,
+        1,
+        4,
+        3,
+        2,
+        4,
+        2,
+        5,
+        5,
+        4
+      ]
+  }
+];
+
+module.exports = friends;
